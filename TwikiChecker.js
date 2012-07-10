917878ef7e403c9be49811d4d2f5ce342f24ce9d
Seth Holloway <smholloway@gmail.com>
2012-07-10 18:16:59 -0500

    Initial commit. Code can be run by pasting it into a JavaScript console attached to a Twiki's WebIndex page. First revisions untouched since 2011 will be highlighted in yellow; entries without an author will be highlighted gray.

---
+++ TwikiChecker.js
@@ -0,0 +1,67 @@
+function markEntries() {
+  var twikiEntries = document.getElementsByClassName('patternSearchResult'), result;
+  for (var i = 0, len = twikiEntries.length; i < len; i++) {
+    twikiEntry = makeTwikiEntryObject(twikiEntries[i]);
+    if ( isStale(twikiEntry) ) markStale(twikiEntry);
+      if ( isOrphaned(twikiEntry) ) markOrphaned(twikiEntry);
+  }
+}
+ 
+function makeTwikiEntryObject(entry) {
+  var result = {};
+
+  result.dom = entry;
+  result.author = getAuthor(entry);
+  result.date = getDate(entry);
+  result.description = getDescription(entry);
+  result.revision = getRevision(entry);
+  result.title = getTitle(entry);
+
+  return result;
+}
+ 
+function getAuthor(entry) {
+  return entry.childNodes[0].childNodes[1].innerText;
+}
+ 
+function getDate(entry) {
+  if ( entry.childNodes[0].childNodes[2].firstChild.innerText ) { // "NEW"
+    return entry.childNodes[0].childNodes[2].childNodes[2].innerText;
+  } else {
+    return entry.childNodes[0].childNodes[2].childNodes[1].innerText;
+  }
+}
+ 
+function getDescription(entry) {
+  return entry.childNodes[2].childNodes[0].innerText;
+}
+ 
+function getRevision(entry) {
+  if ( entry.childNodes[0].childNodes[2].firstChild.innerText ) { // "NEW"
+    return "r0";
+  } else {
+    return entry.childNodes[0].childNodes[2].firstChild.wholeText.split(" ")[0];
+  }
+}
+ 
+function getTitle(entry) {
+  return entry.childNodes[0].childNodes[0].innerText;
+}
+ 
+function isStale(entry) { 
+  return (entry.date < "2011" && entry.revision < "r1");
+}
+ 
+function isOrphaned(entry) {
+  return (entry.author == "UnknownUser" || entry.author == "TWikiContributor");
+}
+ 
+function markStale(entry) {
+  entry.dom.style.background = "yellow";
+}
+ 
+function markOrphaned(entry) {
+  entry.dom.style.background = "grey";
+}
+ 
+markEntries();
